b0038e4791ced2cd9dd4a69ea26aece5290ec44b
Daniel-M <danielmejia55@gmail.com>
2018-06-15 21:01:53 -0500

    Adding more docs

---
+++ examples/brusselator/doc.go
@@ -0,0 +1,47 @@
+// Copyright 2017-2018 Daniel Mejía Raigosa. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+/*
+  .
+
+  The Brusselator
+
+  The Brusselator is a classic example for a oscillating chemical reaction
+  Here you will find several useful examples to get started with this package
+  integrating the Brusselator ODE system. Learn more at https://en.wikipedia.org/wiki/Brusselator
+
+  License
+
+  This code is licensed under MIT license that can be found in the LICENSE
+  file as,
+
+  		MIT License
+
+  		Copyright (c) 2017-2018 Daniel Mejía Raigosa
+
+  		Permission is hereby granted, free of charge, to any person obtaining a copy
+  		of this software and associated documentation files (the "Software"), to deal
+  		in the Software without restriction, including without limitation the rights
+  		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  		copies of the Software, and to permit persons to whom the Software is
+  		furnished to do so, subject to the following conditions:
+
+  		The above copyright notice and this permission notice shall be included in all
+  		copies or substantial portions of the Software.
+
+  		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  		SOFTWARE.
+
+  And at the begining of the source code files as the fragment,
+
+  		Copyright 2017-2018 Daniel Mejía Raigosa. All rights reserved.
+  		Use of this source code is governed by a MIT
+  		license that can be found in the LICENSE file.
+*/
+package odeint
